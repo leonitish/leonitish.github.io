b28c5bdadbc68a9cbb60e34b1d5d2637f351e686
leonitish <leonitish2112@gmail.com>
2025-08-17 17:04:08 +0530

    update and remove packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import './App.css'
 import { useAppContext } from './context/Appcontext'
+import axios from 'axios';
 
 function App() {
   const { user } = useAppContext();
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users/1')
+      .then(response => {
+        console.log('User data fetched:', response.data);
+      }).catch(error => {
+        console.error('Error fetching user data:', error);})
+      }, []);
+
   return (
     <>
       <h1 className={'bg-red'} id={user?.id ?? ''}>Welcome Abishaaaa to My App</h1>
